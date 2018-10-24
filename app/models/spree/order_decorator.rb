e478eab97e197740e0fdf9fdd00ef5becb3be5b0
Eduardo Candanedo <eduardo.candanedo.94@gmail.com>
2018-10-24 11:40:55 -0500

    Stable version for checkout process

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,19 +1,14 @@
 Spree::Order.class_eval do
   checkout_flow do
+    go_to_state :payment, :if => lambda { |order| order.payment_required? }
     go_to_state :confirm, :if => lambda { |order| order.confirmation_required? }
     go_to_state :complete
   end
   
-  def address_required?
-    false
-  end
-
-  # If true, causes the payment step to happen during the checkout process
   def payment_required?
     false
   end
 
-  # If true, causes the confirmation step to happen during the checkout process
   def confirmation_required?
     true
   end
